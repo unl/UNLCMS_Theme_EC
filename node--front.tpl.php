e3905732eb6e0f25a20c78c95142069eeeaf43a1
Seth Meranda <seth@meranda.org>
2013-04-23 12:54:24 -0500

    Add placeholder image to front page.

--- node--front.tpl.php
+++ node--front.tpl.php
@@ -1,4 +1,6 @@
-
+<div class="wdn-band" style="padding-bottom:0; line-height:0;">
+    <img src="<?php echo drupal_get_path('theme', 'explore_center'); ?>/images/front-teaser.jpg" alt="Explore Major promo graphic" />
+</div>
 <?php include 'events.php'; ?>
 <?php print views_embed_view('advisers','band_block', $node->nid); ?>
 <?php print views_embed_view('random_student_story','band_block', $node->nid); ?>
