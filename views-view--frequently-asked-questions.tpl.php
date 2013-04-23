215c795cde406fcd1fe58c3c1ca36cc6b8f32152
Seth Meranda <seth@meranda.org>
2013-04-23 16:04:01 -0500

    Use different selector to avoid collision with WDN grid cols

--- views-view--frequently-asked-questions.tpl.php
+++ views-view--frequently-asked-questions.tpl.php
@@ -1,3 +1,7 @@
-<ul class="faq-list bp2-wdn-grid-set-halves wdn-two-cols wdn-columns-avoid-break">
-    <?php print $rows; ?>
-</ul>
+<section class="<?php print $classes; ?> wdn-band"<?php print $attributes; ?> id="faqs">
+    <div class="wdn-inner-wrapper">
+        <ul class="faq-list bp2-wdn-grid-set-halves wdn-two-flow-columns wdn-flow-columns-avoid-break">
+            <?php print $rows; ?>
+        </ul>
+    </div>
+</div>
