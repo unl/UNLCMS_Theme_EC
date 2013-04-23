06f2befb96bdf4f836736bd52204726fc898f2f1
Seth Meranda <seth@meranda.org>
2013-04-23 09:34:58 -0500

    Add template for pre-law umbrella view.

---
+++ views-view--pre-law-programs.tpl.php
@@ -0,0 +1,13 @@
+<div class="wdn-band <?php print $classes; ?>">
+    <div class="wdn-inner-wrapper">
+        <div class="wdn-grid-set">
+            <div class="bp1-wdn-col-three-fourths">
+                <?php print $header; ?>
+            </div>
+            <div class="bp1-wdn-col-one-fourth bp1-decoration">
+                <h2 class="ec-heading flag"><span>Pre-Law</span> Programs </h2>
+                <?php print $rows; ?>
+            </div>
+        </div>
+    </div>
+</div>
