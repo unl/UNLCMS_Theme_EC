4aa19444e79d093c61e49b3f095196995a5b353f
Seth Meranda <seth@meranda.org>
2013-04-26 16:19:16 -0500

    Reorganize student story teaser for better presentation.

--- node--student-story--teaser.tpl.php
+++ node--student-story--teaser.tpl.php
@@ -1,17 +1,17 @@
  <div class="soothing-wrapper">
     <section id="node-<?php print $node->nid; ?><?php print ($view_mode != 'full' ? '-teaser' : ''); ?>" class="wdn-grid-set student-story-teaser">
         <div class="wdn-col-three-fifths">
-            <h2 class="ec-heading">
-                <span>Student Success</span>
-                <?php print $title; ?>
-                <span class="heading-subhead"><?php print render($content['field_sub_heading']); ?></span>
-            </h2>
             <?php print render($content['field_pull_quote']); ?>
-            <?php print render($content['path']); ?>
-            <a class="button mini" href="<?php print $node_url; ?>">Continue reading <?php print $title; ?>'s story</a>
+            <div class="center">
+                <a class="button mini" href="<?php print $node_url; ?>">Continue reading <?php print $title; ?>'s story</a>
+            </div>
         </div>
         <div class="wdn-col-two-fifths">
             <?php print render($content['field_student_photo']); ?>
+            <h2 class="ec-heading">
+                <?php print $title; ?>
+                <span class="heading-subhead"><?php print render($content['field_sub_heading']); ?></span>
+            </h2>
         </div>
     </section>
 </div> 
