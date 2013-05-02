87ba406b99668efc9937b067bfa9dd3c71b0d31f
Seth Meranda <seth@meranda.org>
2013-05-02 09:47:24 -0500

    Set a breakpoint to avoid mobile cramming.

--- node--student-story--teaser.tpl.php
+++ node--student-story--teaser.tpl.php
@@ -1,12 +1,12 @@
  <div class="soothing-wrapper">
     <section id="node-<?php print $node->nid; ?><?php print ($view_mode != 'full' ? '-teaser' : ''); ?>" class="wdn-grid-set student-story-teaser">
-        <div class="wdn-col-three-fifths">
+        <div class="bp1-wdn-col-three-fifths">
             <?php print render($content['field_pull_quote']); ?>
             <div class="center">
                 <a class="button mini" href="<?php print $node_url; ?>">Continue reading <?php print $title; ?>'s story</a>
             </div>
         </div>
-        <div class="wdn-col-two-fifths">
+        <div class="bp1-wdn-col-two-fifths">
             <?php print render($content['field_student_photo']); ?>
             <h2 class="ec-heading">
                 <?php print $title; ?>
