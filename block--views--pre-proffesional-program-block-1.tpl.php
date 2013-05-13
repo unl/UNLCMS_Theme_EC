3de4e1c8877008835828d2cce3f1c1d921cc3561
Seth Meranda <seth@meranda.org>
2013-05-13 16:23:52 -0500

    More structure to the column block.

---
+++ block--views--pre-proffesional-program-block-1.tpl.php
@@ -0,0 +1,8 @@
+<section class="bp1-wdn-col-one-third" id="pre-professional-programs">
+    <div class="cool-wrapper">
+        <h2 class="ec-heading flag ">
+            <?php print $block->subject; ?>
+        </h2>
+        <?php print $content; ?>
+    </div>
+</section>
--- views-view--pre-health-programs--block-1.tpl.php
+++ views-view--pre-health-programs--block-1.tpl.php
@@ -1,4 +1,3 @@
 <div class="<?php print $classes; ?>">
-    <h2 class="ec-heading flag"><span>Pre-Health</span> Programs </h2>
     <?php print $rows; ?>
 </div>
