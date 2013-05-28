fe42fd93fce6e85be173a8411d095ed6a6ac7692
Seth Meranda <seth@meranda.org>
2013-05-28 12:35:47 -0500

    Rework front page to include major exp and programs.

--- node--front.tpl.php
+++ node--front.tpl.php
@@ -1,5 +1,58 @@
-<div class="wdn-band" style="padding-bottom:0; line-height:0;">
-    <img src="<?php echo drupal_get_path('theme', 'explore_center'); ?>/images/front-teaser.jpg" alt="Explore Major promo graphic" />
+<div class="wdn-band">
+    <div class="wdn-inner-wrapper">
+        <div class="bp1-wdn-grid-set-halves">
+            <div class="wdn-col">
+                <h2 class="ec-heading flag">
+                    Find Your Major
+                </h2>
+                <img src="<?php echo drupal_get_path('theme', 'explore_center'); ?>/images/front-teaser.jpg" alt="Explore Major promo graphic" style="width:100%" />
+                <p>
+                    We're excited to work with you on your exciting journey of exploring and deciding upon a major at UNL! Each student takes a unique path when choosing a major; we'll serve as your guide along the process.
+                </p>
+                <div class="center">
+                    <a class="button width-control soothing" href="#">Find your major</a>
+                </div>
+            </div>
+            <div class="wdn-col">
+                <h2 class="ec-heading flag">
+                    Pre-Professional Programs
+                </h2>
+                <div class="cool-list item-list">    <ul class="wdn-two-flow-columns csscolumns wdn-flow-columns-avoid-break">          <li class="views-row views-row-1 views-row-odd views-row-first">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-chiropractic">Pre-Chiropractic</a></span>  </div></li>
+          <li class="views-row views-row-2 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-clinical-laboratory-science">Pre-Clinical Laboratory Science</a></span>  </div></li>
+          <li class="views-row views-row-3 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-clinical-perfusion-science">Pre-Clinical Perfusion Science</a></span>  </div></li>
+          <li class="views-row views-row-4 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-cytotechnology">Pre-Cytotechnology</a></span>  </div></li>
+          <li class="views-row views-row-5 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-dentistry-hygiene">Pre-Dental Hygiene</a></span>  </div></li>
+          <li class="views-row views-row-6 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-dentistry">Pre-Dentistry</a></span>  </div></li>
+          <li class="views-row views-row-7 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-medicine">Pre-Medicine</a></span>  </div></li>
+          <li class="views-row views-row-8 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health-pre-mortuary-science">Pre-Mortuary Science</a></span>  </div></li>
+          <li class="views-row views-row-9 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-nursing">Pre-Nursing</a></span>  </div></li>
+          <li class="views-row views-row-10 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-occupational-therapy">Pre-Occupational Therapy</a></span>  </div></li>
+          <li class="views-row views-row-11 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-optometry">Pre-Optometry</a></span>  </div></li>
+          <li class="views-row views-row-12 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-pharmacy">Pre-Pharmacy</a></span>  </div></li>
+          <li class="views-row views-row-13 views-row-odd">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-physical-therapy">Pre-Physical Therapy</a></span>  </div></li>
+          <li class="views-row views-row-14 views-row-even">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-physician-assistant">Pre-Physician Assistant</a></span>  </div></li>
+          <li class="views-row views-row-15 views-row-odd views-row-last">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-radiation-science">Pre-Radiation Science</a></span>  </div></li>
+  <li class="views-row views-row-15 views-row-odd views-row-last">  
+  <div class="views-field views-field-title">        <span class="field-content"><a href="http://unlcms.unl.edu/office-of-university-communications/exploratory-and-pre-professional-advising-center/programs/pre-health/pre-radiation-science">Pre-Law</a></span>  </div></li>
+      </ul></div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php include 'events.php'; ?>
 <?php print views_embed_view('advisers','band_block', $node->nid); ?>
