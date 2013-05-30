ca3087d7d9e8ff4acb68ad464626657eba166a96
Seth Meranda <seth@meranda.org>
2013-05-30 09:23:00 -0500

    Add more arguments.

--- node--front.tpl.php
+++ node--front.tpl.php
@@ -1,4 +1,4 @@
-<?php print views_embed_view('homepage_graphics'); ?>
+<?php print views_embed_view('homepage_graphics', 'default', $node->nid); ?>
 <?php include 'events.php'; ?>
 <?php print views_embed_view('advisers','band_block', $node->nid); ?>
 <?php print views_embed_view('random_student_story','band_block', $node->nid); ?>
