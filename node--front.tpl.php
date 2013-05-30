398abaee36099c387b4b38ce6b9ae264df424282
Seth Meranda <seth@meranda.org>
2013-05-30 09:15:31 -0500

    Add new view to the homepage.

--- node--front.tpl.php
+++ node--front.tpl.php
@@ -1,8 +1,4 @@
-<div class="wdn-band">
-    <div class="wdn-inner-wrapper">
-        
-    </div>
-</div>
+<?php print views_embed_view('homepage_graphics'); ?>
 <?php include 'events.php'; ?>
 <?php print views_embed_view('advisers','band_block', $node->nid); ?>
 <?php print views_embed_view('random_student_story','band_block', $node->nid); ?>
