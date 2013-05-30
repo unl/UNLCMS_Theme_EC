d033bc650882a9dbbfd04b0f4d69e54bebb20a9c
Seth Meranda <seth@meranda.org>
2013-05-30 10:22:43 -0500

    File renames.

--- views-view--homepage-graphics--block-1.tpl.php
+++ views-view--homepage-graphics--block-1.tpl.php
@@ -1,15 +1,16 @@
 <div class="wdn-band">
     <div class="wdn-inner-wrapper">
-        <?php print $rows; ?>
+        Rows:
+        <?php //print $rows; ?>
         <div class="bp2-wdn-grid-set-thirds">
             <div class="wdn-col wdn-center">
-                <a href="find-your-major/" class="button ">Find your Major</a>
+                <a href="find-your-major/" class="button soothing">Find your Major</a>
             </div>
             <div class="wdn-col wdn-center">
-                <a href="programs/pre-health" class="button">Pre-Health</a>
+                <a href="programs/pre-health" class="button soothing">Pre-Health</a>
             </div>
             <div class="wdn-col wdn-center">
-                <a href="programs/pre-law" class="button">Pre-Law</a>
+                <a href="programs/pre-law" class="button soothing">Pre-Law</a>
             </div>
         </div>
     </div>
