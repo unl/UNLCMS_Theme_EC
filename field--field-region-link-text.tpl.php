d8be7727bdff1a6bb841fb4d0d7703ae8b6fde2d
Seth Meranda <seth@meranda.org>
2013-06-07 11:27:38 -0500

    Add `now-selected` to first link automatically.

--- field--field-region-link-text.tpl.php
+++ field--field-region-link-text.tpl.php
@@ -2,9 +2,16 @@
 <?php
 $i = 0;
 $nid = $element['#object']->nid;
-foreach ($items as $item) { ?>
+foreach ($items as $item) { 
+    if ($i == 0) {
+        $class = "now-selected";
+    } else {
+        $class = "";
+    }
+?>
+
     <li>
-        <span class="fake-modal ec-section-link" data-tab="#region-<?php echo $nid . '-' .$i; ?>">
+        <span class="fake-modal ec-section-link <?php echo $class; ?>" data-tab="#region-<?php echo $nid . '-' .$i; ?>">
             <?php print render($item['#markup']); ?>
         </a>
     </li>
