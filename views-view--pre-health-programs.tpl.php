7547618d50b74f6b3d348709dfb04db1cf1da610
Seth Meranda <seth@meranda.org>
2013-06-12 08:20:39 -0500

    Remove holder text.

--- views-view--pre-health-programs.tpl.php
+++ views-view--pre-health-programs.tpl.php
@@ -5,7 +5,7 @@
                 <?php print $header; ?>
             </div>
             <div class="bp1-wdn-col-one-fourth bp1-decoration">
-                We need a photo here? (note, this is a decoration area and hides in mobile)
+                
             </div>
         </div>
     </div>
