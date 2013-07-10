e8e705ca19f25540bf90e13715026e9d31ea283d
Seth Meranda <seth@meranda.org>
2013-07-10 16:51:02 -0500

    Cleaner mobile layouts for student stories.

--- node--student-story.tpl.php
+++ node--student-story.tpl.php
@@ -1,11 +1,11 @@
 <h2 class="ec-heading"><span><?php print render($content['field_sub_heading']); ?></span></h2>
 <section id="node-<?php print $node->nid; ?><?php print ($view_mode != 'full' ? '-teaser' : ''); ?>" class="wdn-grid-set">
-    <div class="bp1-wdn-col-two-fifths">
+    <div class="bp2-wdn-col-two-fifths">
         <p class="summary">
             <?php print render($content['body']['#items'][0]['summary']); ?>
         </p>
     </div>
-    <div class="bp1-wdn-col-three-fifths">
+    <div class="bp2-wdn-col-three-fifths">
         <?php print render($content['field_student_photo']); ?>
     </div>
 </section>
